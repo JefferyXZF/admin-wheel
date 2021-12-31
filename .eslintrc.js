ab4be136aea2df02bb53fc853770b99c8376c0f7
xiezhifei <xiezhifei@do1.com.cn>
2021-12-31 20:42:20 +0800

    ci: 修改ESLint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,18 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-		'plugin:prettier/recommended', // Prettier规则集
+    'plugin:vue/essential', // Vue 规则
+    'airbnb-base', // airbnb 规范
+    'plugin:prettier/recommended', // Prettier规则集
   ],
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser', // .ts 文件解析器，帮助ESLint解析typescript文件
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-		'prettier'
-  ],
-	// 自定义规则，配置后会覆盖extends中已有的规则，官方规则配置手册: https://eslint.bootcss.com/docs/rules/
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  // 自定义规则，配置后会覆盖extends中已有的规则，官方规则配置手册: https://eslint.bootcss.com/docs/rules/
   rules: {
-		'prettier/prettier': 'error',
+    'prettier/prettier': 'error',
   },
 }
